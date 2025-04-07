63bd212a568c128c517779f627d386b97e53ae38
even-wei <evenwei@infuseai.io>
2025-04-07 17:21:32 +0800

    Remove target base and add it to gitignore
    Signed-off-by: even-wei <evenwei@infuseai.io>

--- target-base/compiled/jaffle_shop/models/customer_order_pattern.sql
+++
@@ -1,9 +0,0 @@
--- Analyzing Order Patterns
-SELECT
-    customer_id,
-    first_order,
-    most_recent_order,
-    number_of_orders,
-    DATEDIFF('day', first_order, most_recent_order) AS days_active,
-    DATEDIFF('day', first_order, most_recent_order) / NULLIF(number_of_orders - 1, 0) AS avg_days_between_orders
-FROM "jaffle_shop"."prod"."customers"
